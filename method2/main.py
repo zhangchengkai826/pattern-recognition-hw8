b57f73ed21fded91b810db826e75e7969d2f5e4e
ZhengChengkai826 <zchengkai826@gmail.com>
2019-11-22 11:36:08 +0800

    first try

--- method2/main.py
+++ method2/main.py
@@ -14,6 +14,9 @@ IMG_SIZE = 32
 tmp_cnt = 0
 tmp_dir = 'tmp'
 
+DIVISION = 0.2
+ONE_RATIO_THRESHOLD = 0.5
+
 def preprocess(im):
     global tmp_cnt
 
@@ -77,6 +80,47 @@ def preprocess(im):
     # plt.show()
     return im
 
+# 对图片第[s, e]行进行特征提取
+# 出现只有一组连续点的行数超过50%，返回1
+# 出现只有一组连续点的行，但只有一组连续点的行数未超过50%，返回2
+# 未出现只有一组连续点的行，返回3
+# 注意：如果有一行没有出现任何连续点，则不纳入行数统计
+# 若将图像划分为上中下三部分（中间部分较大，上下部分较小）
+# 则理想状况下：
+# 勾 - [1 1 1], 叉 - [1/3 2 1/3], 圈 - [2/3 3 2/3]
+# 所以只需要看特征向量的第2个分量即可
+# 此时，分类准确率为76.54%
+# 错误分类信息详见error.log
+def extrack_feature(im, s, e):
+    total = e-s+1
+    one = 0
+    for y in range(s, e+1):
+        cnt = 0
+        met_black = False
+        for x in range(IMG_SIZE):
+            if not met_black:
+                if im[y][x] == 0:
+                    met_black = True
+            else:
+                if im[y][x] == 1:
+                    met_black = False
+                    cnt += 1
+        if met_black:
+            cnt += 1
+        
+        if cnt == 0:
+            total -= 1
+        elif cnt == 1:
+            one += 1
+
+    if total == 0 or one == 0:
+        return 3
+    ratio = one / total
+    if ratio > ONE_RATIO_THRESHOLD:
+        return 1
+    else:
+        return 2
+
 if __name__ == '__main__':
     data_dir = 'data'
     im_prefix = 'data_binary'
@@ -92,4 +136,27 @@ if __name__ == '__main__':
             for l in open('{}\\{}'.format(data_dir, file_name), 'r'):
                 labels.append(int(l))
     
+    dh = int(IMG_SIZE * DIVISION)
+    idx = 0
+    correct = 0
+    for im in images:
+        # 特征提取
+        v = []
+        v.append(extrack_feature(im, 0, dh-1))
+        v.append(extrack_feature(im, dh, IMG_SIZE-dh-1))
+        v.append(extrack_feature(im, IMG_SIZE-dh, IMG_SIZE-1))
 
+        # 识别分类统计
+        # 0 - 勾, 1 - 叉，2 - 圈
+        mapping = [None, 0, 1, 2]
+        prediction = mapping[v[1]]
+
+        if prediction == labels[idx]:
+            correct += 1
+        else:
+            print("Features: {}".format(v))
+            print("Prediction: {}, Label: {}".format(prediction, labels[idx]))
+            # plt.imshow(im)
+            # plt.show()
+        idx += 1
+    print("Accuracy: {0:.2f}%".format(correct / len(labels) * 100))
